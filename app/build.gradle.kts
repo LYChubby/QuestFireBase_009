6448e77111b4f865376c6184b0fd8782ae10fe57
LYChubby <f4uzi345@gmail.com>
2025-01-08 18:06:17 +0700

    Menambah Gradlle dan Libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-analytics")
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
