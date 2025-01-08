94b28eb467c2b904adad0df68ab7d5e24ae49fbd
LYChubby <f4uzi345@gmail.com>
2025-01-08 17:24:04 +0700

    Menambah Fungsi HomeViewModel

---
+++ app/src/main/java/com/example/mvvmfirebase/ui/home/viewmodel/HomeViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.mvvmfirebase.ui.home.viewmodel
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvmfirebase.model.Mahasiswa
+import com.example.mvvmfirebase.repository.RepositoryMhs
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+class HomeViewModel (
+    private val repositoryMhs: RepositoryMhs
+): ViewModel() {
+
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun getMhs() {
+        viewModelScope.launch {
+            repositoryMhs.getAllMhs()
+                .onStart {
+                    mhsUIState = HomeUiState.Loading
+                }
+                .catch {
+                    mhsUIState = HomeUiState.Error(e = it)
+                }
+                .collect {
+                    mhsUIState = if (it.isEmpty()) {
+                        HomeUiState.Error(Exception("Belum Ada Data Mahasiswa"))
+                    } else {
+                        HomeUiState.Success(it)
+                    }
+                }
+        }
+    }
+}
+
+sealed class HomeUiState {
+   object Loading : HomeUiState()
+
+    data class Success(val data: List<Mahasiswa>) : HomeUiState()
+    data class Error(val e: Throwable) : HomeUiState()
+}
