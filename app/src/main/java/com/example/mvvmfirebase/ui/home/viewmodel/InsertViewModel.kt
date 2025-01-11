7a9a6a93ecc0360f16582ef9ee575631a100e952
LYChubby <f4uzi345@gmail.com>
2025-01-11 20:59:36 +0700

    Membuat FormState, InsertUiState, FormErrorState, MahasiswaEvent & Fungsi Untuk Menyimpan Input

--- app/src/main/java/com/example/mvvmfirebase/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/mvvmfirebase/ui/home/viewmodel/InsertViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.mvvmfirebase.ui.home.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.mvvmfirebase.model.Mahasiswa
 import com.example.mvvmfirebase.repository.RepositoryMhs
 
 sealed class FormState {
@@ -9,3 +10,41 @@ sealed class FormState {
     data class Success(val message: String) : FormState()
     data class Error(val message: String) : FormState()
 }
+
+data class InsertUiState (
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState()
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val alamat: String? = null,
+    val jenisKelamin: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && alamat == null &&
+                jenisKelamin == null && kelas == null && angkatan == null
+    }
+}
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+// Menyimpan Input Form Ke Dalam Entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
