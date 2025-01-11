3963058bf6b75017dd9075b36d3bc915483c0fdf
LYChubby <f4uzi345@gmail.com>
2025-01-11 19:23:41 +0700

    Menambah Fungsi Untuk Update Di NetworkRepositoryMhs

--- app/src/main/java/com/example/mvvmfirebase/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/mvvmfirebase/repository/NetworkRepositoryMhs.kt
@@ -20,7 +20,14 @@ class NetworkRepositoryMhs (
     }
 
     override suspend fun updateMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .set(mahasiswa)
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal Mengupdate Data Mahasiswa : ${e.message}")
+        }
     }
 
     override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
