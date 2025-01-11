0f49de9152a4a0041f47d94ed0e3027f2e8b2c66
LYChubby <f4uzi345@gmail.com>
2025-01-11 22:09:59 +0700

    Membuat Fungsi InsertBodyMhs

--- app/src/main/java/com/example/mvvmfirebase/ui/home/pages/InsertView.kt
+++ app/src/main/java/com/example/mvvmfirebase/ui/home/pages/InsertView.kt
@@ -6,12 +6,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -19,10 +23,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.mvvmfirebase.ui.home.viewmodel.FormErrorState
+import com.example.mvvmfirebase.ui.home.viewmodel.FormState
+import com.example.mvvmfirebase.ui.home.viewmodel.HomeUiState
+import com.example.mvvmfirebase.ui.home.viewmodel.InsertUiState
 import com.example.mvvmfirebase.ui.home.viewmodel.MahasiswaEvent
 
 @Composable
@@ -168,3 +176,44 @@ fun FormMahasiswa(
         )
     }
 }
+
+@Composable
+fun InsertBodyMhs (
+    modifier: Modifier = Modifier,
+    onValueChange: (MahasiswaEvent) -> Unit,
+    uiState: InsertUiState,
+    onClick: () -> Unit,
+    homeUiState: FormState
+) {
+
+    Column (
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        FormMahasiswa(
+            mahasiswaEvent = uiState.insertUiEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = homeUiState !is FormState.Loading
+        ) {
+            if (homeUiState is FormState.Loading) {
+                CircularProgressIndicator(
+                    color = Color.White,
+                    modifier = Modifier
+                        .size(20.dp)
+                        .padding(8.dp)
+                )
+                Text("Loading.....")
+            } else {
+                Text("Add")
+            }
+        }
+    }
+}
+
