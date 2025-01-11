5e7417a3e7a2bbdbaa4f8b9857a070db82497960
LYChubby <f4uzi345@gmail.com>
2025-01-11 19:32:00 +0700

    Menambah Fungsi Delete Pada HomeViewModel

--- app/src/main/java/com/example/mvvmfirebase/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/mvvmfirebase/ui/home/viewmodel/HomeViewModel.kt
@@ -44,6 +44,16 @@ class HomeViewModel (
                 }
         }
     }
+
+    fun deleteMhs(mahasiswa: Mahasiswa) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMhs(mahasiswa)
+            } catch (e: Exception) {
+                mhsUIState = HomeUiState.Error(e)
+            }
+        }
+    }
 }
 
 sealed class HomeUiState {
