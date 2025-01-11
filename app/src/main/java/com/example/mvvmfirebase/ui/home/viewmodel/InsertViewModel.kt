1987d845d061e64348794f252832bf70d6441628
LYChubby <f4uzi345@gmail.com>
2025-01-11 21:06:48 +0700

    Mmebuat Fungsi InsertViewModel

--- app/src/main/java/com/example/mvvmfirebase/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/mvvmfirebase/ui/home/viewmodel/InsertViewModel.kt
@@ -1,8 +1,72 @@
 package com.example.mvvmfirebase.ui.home.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mvvmfirebase.model.Mahasiswa
 import com.example.mvvmfirebase.repository.RepositoryMhs
+import kotlinx.coroutines.launch
+
+class InsertViewModel (
+    private val msh: RepositoryMhs
+) : ViewModel() {
+
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // Memperbarui State Berdasarkan Input Pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent
+        )
+    }
+
+    fun validateFields() : Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isEmpty()) "NIM Tidak Boleh Kosong" else null,
+            nama = if (event.nama.isEmpty()) "Nama Tidak Boleh Kosong" else null,
+            alamat = if (event.alamat.isEmpty()) "Alamat Tidak Boleh Kosong" else null,
+            jenisKelamin = if (event.jenisKelamin.isEmpty()) "Jenis Kelamin Tidak Boleh Kosong" else null,
+            kelas = if (event.kelas.isEmpty()) "Kelas Tidak Boleh Kosong" else null,
+            angkatan = if (event.angkatan.isEmpty()) "Angkatan Tidak Boleh Kosong" else null
+        )
+
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun insertMhs() {
+
+        if (validateFields()) {
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    msh.insertMhs(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data Berhasil Disimpan")
+                } catch (e: Exception) {
+                    uiState = FormState.Error("Data Gagal Disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data Tidak Valid")
+        }
+    }
+
+    fun resetForm() {
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+
+    fun resetSnackBarMessage() {
+        uiState = FormState.Idle
+    }
+}
 
 sealed class FormState {
     object Idle : FormState()
