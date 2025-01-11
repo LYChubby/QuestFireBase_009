ff3e4ba37ecb45221b80d28231cb78015c0e8b7a
LYChubby <f4uzi345@gmail.com>
2025-01-11 20:53:28 +0700

    Menambah Fungsi Delete Pada HomeViewModel

--- app/src/main/java/com/example/mvvmfirebase/ui/home/pages/HomeView.kt
+++ app/src/main/java/com/example/mvvmfirebase/ui/home/pages/HomeView.kt
@@ -246,7 +246,7 @@ fun HomeScreen(
             retryAction = { viewModel.getMhs() },
             modifier = Modifier.padding(innerPadding),
             onDetailClick = onDetailClick, onDeleteClick = {
-                viewModel.getMhs()
+                viewModel.deleteMhs(it)
             }
         )
     }
--- app/src/main/java/com/example/mvvmfirebase/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/mvvmfirebase/ui/home/viewmodel/HomeViewModel.kt
@@ -48,7 +48,7 @@ class HomeViewModel (
     fun deleteMhs(mahasiswa: Mahasiswa) {
         viewModelScope.launch {
             try {
-                mhs.deleteMhs(mahasiswa)
+                repositoryMhs.deleteMhs(mahasiswa)
             } catch (e: Exception) {
                 mhsUIState = HomeUiState.Error(e)
             }
