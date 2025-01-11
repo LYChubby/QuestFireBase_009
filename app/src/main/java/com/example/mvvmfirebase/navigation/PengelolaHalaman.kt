20913405ccbb72b0ba3ae8d1766eeb427355b4b0
LYChubby <f4uzi345@gmail.com>
2025-01-11 22:29:06 +0700

    Membuat Fungsi PengelolaHalaman

---
+++ app/src/main/java/com/example/mvvmfirebase/navigation/PengelolaHalaman.kt
@@ -0,0 +1,42 @@
+package com.example.mvvmfirebase.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mvvmfirebase.ui.home.pages.HomeScreen
+import com.example.mvvmfirebase.ui.home.pages.InsertMhsView
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToitemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route) {
+            InsertMhsView(
+                onBack = {navController.popBackStack()},
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
